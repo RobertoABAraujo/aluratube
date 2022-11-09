d7611295dc21082aab34497c0c0538eafc4deeb6
robertoa <robertoa@agap2.pt>
2022-11-09 16:16:02 +0000

    update_search

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
@@ -5,30 +6,36 @@ import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-  const estiloDaHomePage = { 
-    //backgroundColor: "red" 
+  const estilosDaHomePage = {
+    // backgroundColor: "red"
   };
+  const [filterValue, setFilterValue] = React.useState("");
 
   return (
     <>
       <CSSReset />
-      <div style={{
-                display: "flex",
-                flexDirection: "column",
-                flex: 1,
-                // backgroundColor: "red",
-            }}>
-        <Menu />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+        }}
+      >
+        {/* Prop Drilling */}
+        <Menu
+          filterValue={filterValue}
+          setFilterValue={setFilterValue}
+        />
         <Header />
-        <Timeline playlists={config.playlists}>
-            Conteúdo
+        <Timeline searchValue={filterValue} playlists={config.playlists}>
+          Conteúdo
         </Timeline>
       </div>
     </>
   );
 }
 
-export default HomePage
+export default HomePage;
 
 const StyledHeader = styled.div`
   img {
@@ -36,21 +43,25 @@ const StyledHeader = styled.div`
     height: 80px;
     border-radius: 50%;
   }
-
   .user-info {
-        margin-top: 50px;
-        display: flex;
-        align-items: center;
-        width: 100%;
-        padding: 16px 32px;
-        gap: 16px;
-    }
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
+
+const StyledBanner = styled.div`
+  background-color: blue;
+  background-image: url(${({ bg }) => bg});
+  height: 230px;
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      {/*<img src="banner" />*/}
+      <StyledBanner bg={config.bg} />
       <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
         <div>
@@ -62,26 +73,32 @@ function Header() {
   );
 }
 
-function Timeline(props) {
+function Timeline({ searchValue, ...props }) {
   const playlistNames = Object.keys(props.playlists);
-  //Statement
+  // Statement
   // Retorno por expressão
   return (
     <StyledTimeline>
       {playlistNames.map((playlistName) => {
         const videos = props.playlists[playlistName];
         return (
-          <section>
+          <section key={playlistName}>
             <h2>{playlistName}</h2>
             <div>
-              {videos.map((video) => {
-                return (
-                  <a href={video.url}>
-                    <img src={video.thumb} />
-                    <span>{video.title}</span>
-                  </a>
-                );
-              })}
+              {videos
+                .filter((video) => {
+                  const titleNormalized = video.title.toLowerCase();
+                  const searchValueNormalized = searchValue.toLowerCase();
+                  return titleNormalized.includes(searchValueNormalized);
+                })
+                .map((video) => {
+                  return (
+                    <a key={video.url} href={video.url}>
+                      <img src={video.thumb} />
+                      <span>{video.title}</span>
+                    </a>
+                  );
+                })}
             </div>
           </section>
         );
